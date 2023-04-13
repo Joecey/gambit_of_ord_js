9cd9c97cec9f5bffa59678a72f12be8f7a1e0ec6
Joe Linogao <linogaoj@tcd.ie>
2023-04-13 21:15:48 +0100

    added dom manipulation

---
+++ js/dice.js
@@ -0,0 +1,3 @@
+// function will be used to store dice functions
+// inputs > dice type
+// outputs > roll
--- js/index.js
+++ js/index.js
@@ -5,21 +5,34 @@ console.log("Hello world");
 let player_roll = 0;
 let computer_roll = 0;
 
+const player_dice = document.getElementById("playerdice");
+const computer_dice = document.getElementById("compdice");
+let player_text = "";
+let computer_text = "";
+
 document.querySelector("#player_dice").addEventListener("click", function () {
 	// choose dice based on mod (0,1,2,0,1,2 etc etc)
 	console.log(player_roll);
 	const dice_num = player_roll % 3;
+
 	switch (dice_num) {
 		case 0:
-			console.log("d8 rolled for player");
+			player_text = "d8 rolled for player";
+
 			break;
 		case 1:
-			console.log("d6 rolled for player");
+			player_text = "d6 rolled for player";
+
 			break;
 		case 2:
-			console.log("d4 rolled for player");
+			player_text = "d4 rolled for player";
+
 			break;
 	}
+
+	// display current dice rolled and change text
+	console.log(player_text);
+	player_dice.innerHTML = player_text;
 
 	// increment dice roll by 1
 	player_roll++;
@@ -32,15 +45,18 @@ document.querySelector("#computer_dice").addEventListener("click", function () {
 	const dice_num = computer_roll % 3;
 	switch (dice_num) {
 		case 0:
-			console.log("d8 rolled for computer");
+			computer_text = "d8 rolled for computer";
 			break;
 		case 1:
-			console.log("d6 rolled for computer");
+			computer_text = "d6 rolled for computer";
 			break;
 		case 2:
-			console.log("d4 rolled for computer");
+			computer_text = "d4 rolled for computer";
 			break;
 	}
+
+	console.log(computer_text);
+	computer_dice.innerHTML = computer_text;
 
 	// increment dice roll by 1
 	computer_roll++;
