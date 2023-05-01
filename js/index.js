09c6feb5cf21e13e5d0b69a8fc8763732669dd9c
Joe Linogao <linogaoj@tcd.ie>
2023-05-01 18:24:48 +0100

    added score logic

--- js/index.js
+++ js/index.js
@@ -4,40 +4,92 @@
 import { diceRoll } from "./dice.js";
 
 // add event listener for button roll
+
+let game_state = false;
 let player_roll = 0;
-let text_roll = "";
+let player_number = 0;
+let computer_number = 0;
+let player_total = 0;
+let comp_total = 0;
 
 const player_dice = document.getElementById("playerdice");
 const computer_dice = document.getElementById("compdice");
+const state_text = document.getElementById("game_state");
+const player_total_text = document.getElementById("player_total");
+const comp_total_text = document.getElementById("comp_total");
+const html_result = document.getElementById("result");
+
+document.querySelector("#button_state").addEventListener("click", function () {
+	// switch booleon
+	game_state = !game_state;
+	state_text.innerHTML = game_state;
+});
 
 document.querySelector("#player_dice").addEventListener("click", function () {
 	// choose dice based on mod (0,1,2,0,1,2 etc etc)
 	// console.log(player_roll);
-	const dice_num = player_roll % 3;
+	if (game_state === true) {
+		const dice_num = player_roll % 3;
 
-	switch (dice_num) {
-		case 0:
-			text_roll = "d8 rolled";
-			console.log(diceRoll(8));
+		switch (dice_num) {
+			case 0:
+				// reset total here
+				player_total = 0;
+				comp_total = 0;
 
-			break;
-		case 1:
-			text_roll = "d6 rolled";
-			console.log(diceRoll(6));
+				console.log("d8 rolled");
+				// roll for playerr
+				player_number = diceRoll(8);
+				// roll for computer
+				computer_number = diceRoll(8);
 
-			break;
-		case 2:
-			text_roll = "d4 rolled";
-			console.log(diceRoll(4));
+				break;
+			case 1:
+				console.log("d6 rolled");
+				// roll for playerr
+				player_number = diceRoll(6);
+				// roll for computer
+				computer_number = diceRoll(6);
 
-			break;
+				break;
+			case 2:
+				console.log("d4 rolled");
+				// roll for playerr
+				player_number = diceRoll(4);
+				// roll for computer
+				computer_number = diceRoll(4);
+				// change game state back to false
+				game_state = !game_state;
+
+				break;
+		}
+
+		// display current dice rolled and change text
+		// console.log(text_roll);
+		player_dice.innerHTML = player_number;
+		computer_dice.innerHTML = computer_number;
+
+		// update total
+		player_total = player_total + player_number;
+		comp_total = comp_total + computer_number;
+		player_total_text.innerHTML = player_total;
+		comp_total_text.innerHTML = comp_total;
+
+		// increment dice roll by 1
+		player_roll++;
+
+		// update game state on screen
+		state_text.innerHTML = game_state;
+
+		// check final score here
+		if (dice_num === 2) {
+			if (player_total > comp_total) {
+				html_result.innerHTML = "Player wins";
+			} else if (player_total < comp_total) {
+				html_result.innerHTML = "Computer wins";
+			} else {
+				html_result.innerHTML = "Draw!";
+			}
+		}
 	}
-
-	// display current dice rolled and change text
-	// console.log(text_roll);
-	player_dice.innerHTML = text_roll;
-	computer_dice.innerHTML = text_roll;
-
-	// increment dice roll by 1
-	player_roll++;
 });
